682d9ba1d1177c94388ab73d19a22906d879260c
Jamal Turner <jlsmitty2273@yahoo.com>
2020-01-18 06:41:51 -0500

    updated the controller but theres a hidden error

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,25 @@
 class GamesController < ApplicationController
 
+    #create
+    
+    #
+    get '/games/new' do 
+        erb :'/games/new'
+    end
+    
+    #something here throws an error
 
+    post '/games' do 
+        game = Game.new(params)
+        game.save
+        if !game.title.empty? && !game.type.empty? && !game.rating.empty?
+            game.save
+            redirect '/games'
+        else
+            @error = "Data invalid. Please try again"
+            erb :'/games/new'
+        end
+    end
 
 
     #read
