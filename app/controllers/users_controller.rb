3955b9333f8e9d55070df23f167c146877a87561
Jamal Turner <jlsmitty2273@yahoo.com>
2020-01-21 01:52:20 -0500

    added user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     get '/signup' do 
-        erb :'users/signup.erb'
+        erb :'users/signup'
     end
 
     post '/signup' do 
@@ -9,8 +9,12 @@ class UsersController < ApplicationController
         if user.username.empty? || user.password.empty?
             @error = "Username and password can't be blank"
             erb :'users/signup'  
+        elsif User.find_by(username: user.username)
+            @error = "Account with that username already exists"
+            erb :'users/signup'
         else
             user.save
+            session[:user_id] = user.id
             redirect '/games'
         end    
     end
