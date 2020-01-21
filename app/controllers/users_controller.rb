2558acfc0799a0a93ea05e8a4c5fba6c555d4abe
Jamal Turner <jlsmitty2273@yahoo.com>
2020-01-20 23:58:19 -0500

    setting the users and made a basic navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
 
+    get '/signup' do 
+        erb :'users/signup.erb'
+    end
 
+    post '/signup' do 
+        user = User.new(params) 
+        if user.username.empty? || user.password.empty?
+            @error = "Username and password can't be blank"
+            erb :'users/signup'  
+        else
+            user.save
+            redirect '/games'
+        end    
+    end
 end
